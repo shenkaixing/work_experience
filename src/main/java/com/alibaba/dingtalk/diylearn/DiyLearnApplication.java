b8b6cead02582bee85efddf2754808ef32c3bc83
shenkaixing <1406897577@qq.com>
2022-04-22 00:32:32 +0800

    init "hello world"

---
+++ src/main/java/com/alibaba/dingtalk/diylearn/DiyLearnApplication.java
@@ -0,0 +1,14 @@
+package com.alibaba.dingtalk.diylearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@SpringBootApplication
+public class DiyLearnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiyLearnApplication.class, args);
+	}
+}
---
+++ src/test/java/com/alibaba/dingtalk/diylearn/DiyLearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alibaba.dingtalk.diylearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiyLearnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
