bcce86847038f99dcaefc751fe0f50e8cd837b2b
shenkaixing.skx <shenkaixing.skx@alibaba-inc.com>
2023-08-21 19:19:54 +0800

    to #41378463 幂等处理

--- src/main/java/com/lanya/controller/IdempotentController.java
+++ src/main/java/com/lanya/controller/IdempotentController.java
@@ -1,5 +1,7 @@
 package com.lanya.controller;
 
+import java.util.concurrent.TimeUnit;
+
 import com.lanya.dao.vo.DeptVO;
 import com.pig4cloud.plugin.idempotent.annotation.Idempotent;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -8,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 /**
  * 幂等接口尝试
+ * 参考文档：https://www.cnblogs.com/guangwenyin/p/15689210.html
  * 参考链接：https://github.com/pig-mesh/idempotent-spring-boot-starter
  * @author 兰崖 shenkaixing.skx
  * @date 2023/8/17 5:51 下午
@@ -19,7 +22,7 @@ public class IdempotentController {
      * @param deptVO
      * @return
      */
-    @Idempotent(key = "#deptVO.name", expireTime = 30, info = "部门重复")
+    @Idempotent(key = "#deptVO.name", expireTime = 30,timeUnit = TimeUnit.MINUTES ,info = "部门重复")
     @PostMapping("/ide/test")
     public String test(@RequestBody DeptVO deptVO) {
         return deptVO.getName();
